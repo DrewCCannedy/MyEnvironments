196b5ee0a7f7d2ff34fd80878a359e09b9dfb704
Drew Cannedy <drew.cannedy@respec.com>
2019-11-04 14:33:00 -0600

    add java

--- Linux/setup_env.sh
+++ Linux/setup_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # setup linux environment with things I use
 
+# is this for work or personal use?
 echo "1) Work or 2) Personal: "
 read work_or_personal
 if [ $work_or_personal="1" ];
@@ -27,6 +28,14 @@ sudo apt-get -y install git
 git config --global user.name "Drew Cannedy"
 git config --global user.email $email
 
+# java
+apt-get -y install default-jdk
+java_path="$(readlink -f $(which java))"
+java_path=${java_path%"bin/java"}
+echo -e "\nJAVA_HOME=$java_path" | sudo tee -a /etc/environment 
+source /etc/environment
+
+# cleanup
 sudo apt-get -y install -f
 sudo apt -y autoremove
 echo -e "\nProcess Completed"
